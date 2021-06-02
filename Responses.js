24560dca41df3b91ce564c31d3885e22d5f1efb9
franciane-brito <francianebrito99@gmail.com>
2021-06-02 17:35:21 -0300

    Resposta 1

---
+++ Responses.js
@@ -0,0 +1,11 @@
+// 1) RESPOSTA
+
+function newArray(quantidade, item){
+    let data = []
+        for(i = 0; i < quantidade; i++){
+    data.push(item)
+    }
+     return data
+    }
+    
+    newArray(3, "a")
